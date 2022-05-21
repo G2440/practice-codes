112624e4159d27ba5ba019eb8baf3646105313aa
Gaurav Negi <42696032+G2440@users.noreply.github.com>
2022-05-21 23:12:33 +0530

    Update recursion_prac.cpp

--- recursion_prac.cpp
+++ recursion_prac.cpp
@@ -7,3 +7,9 @@ void func(int* arr, int i,int j){
     arr[j] = temp;
     func(arr,i+1,j-1);
 }
+//MULTIPLE CALL RECURSION FOR FIBONACCI
+
+int fibo(int x){
+   if(x<=1)return x;
+   return fibo(x-1) + fibo(x-2);
+}
